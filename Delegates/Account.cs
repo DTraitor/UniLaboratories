2810be90d20b7aea3ca79a0c895b607dc163d5c1
DTraitor <sehhd32@gmail.com>
2023-12-07 23:54:30 +0200

    THAT'S IT!? THAT'S THE WHOLE LABORATORY WORK!?

---
+++ Delegates/Account.cs
@@ -0,0 +1,40 @@
+﻿namespace Delegates;
+
+public class Account
+{
+    public delegate void OnAccountEmpty();
+    public event OnAccountEmpty Notify;
+
+    public int Sum { get; private set; }
+    public int HoursTalked { get; private set; } = 0;
+    public int TariffCost { get; private set; }
+
+    public Account(int sum, int tariffCost)
+    {
+        Sum = sum;
+        TariffCost = tariffCost;
+    }
+
+    public void PayTariff()
+    {
+        Take(TariffCost);
+    }
+
+    public void Put(int sum)
+    {
+        Sum += sum;
+    }
+
+    public void Take(int sum)
+    {
+        if (Sum >= sum)
+            Sum -= sum;
+        if (Sum == 0)
+            Notify?.Invoke();
+    }
+
+    public void Talk(int hours)
+    {
+        HoursTalked += hours;
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿// Отримання зі значень головної діагоналі двовимірного рядка одновимірного рядка
+
+using Delegates;
+
+Main();
+
+void Main()
+{
+    string[] array =
+    {
+        "Change",
+        "da",
+        "world.",
+        "My",
+        "final",
+        "message.",
+        "Goodbye."
+    };
+
+    GetDiagonal getDiagonal = delegate(string[] arr)
+    {
+        string diagonal = "";
+        for (int i = 0; i < arr.GetLength(0); i++)
+            diagonal += arr[i][arr[i].Length / 2];
+        return diagonal;
+    };
+
+    // Result: nalynab
+    string diagonalArray = getDiagonal(array);
+    Console.WriteLine(diagonalArray);
+
+    // Використовуючи створений у п.3 компонент, створити додаток, у якому визначити метод- обробник події для цього компонента, що реалізує реакцію додатка на подію (наприклад, повідомлення користувачеві про виникнення події). Метод-обробник події повинний отримувати інформацію про об’єкт-ініціатор події та аргумент події.**
+    Account account = new Account(100, 50);
+    account.Notify += () => Console.WriteLine("Account is empty!");
+    account.PayTariff();
+    account.PayTariff();
+    account.PayTariff();
+    // Result: Account is empty!
+}
+
+delegate string GetDiagonal(string[] array);
